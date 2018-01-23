0c9ddf411801592ba2fb9085e90a81d572c10b24
Neal Phelps <nphelpsiv@gmail.com>
2018-01-22 21:20:31 -0700

    more refactoring

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,16 +3,13 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const tours = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, name: 'Neals Tour' },
+      { id: 2, name: 'Bens Tours' },
+      { id: 3, name: 'Chris Tour' },
+      { id: 4, name: 'Taylors tour' },
+      { id: 5, name: 'Jims Tour' },
+      { id: 6, name: 'The Best Tour' },
+      { id: 7, name: 'Aquarium' }
     ];
     return {tours};
   }
